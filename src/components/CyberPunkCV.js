7110febffa5b8b6997598f11ae8951bc39c37f5e
dlobregon <troyanrex@gmail.com>
2023-06-23 12:02:12 -0600

    add initial strcuture - draft

---
+++ src/components/CyberPunkCV.js
@@ -0,0 +1,14 @@
+const CyberPunkCV = () =>{
+    return (
+        <div>
+            <h1>Diego Lopez Obregon - 2</h1>
+            <ul>
+                <li>Experience - 2</li>
+                <li>Education - 2</li>
+                <li>Skills - 2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default CyberPunkCV
---
+++ src/components/SimpleCV.js
@@ -0,0 +1,14 @@
+const SimpleCV = ()=> {
+    return (
+        <div>
+            <h1>Diego Lopez Obregon</h1>
+            <ul>
+                <li>Experience</li>
+                <li>Education</li>
+                <li>Skills</li>
+            </ul>
+        </div>
+    )
+}
+
+export default SimpleCV
